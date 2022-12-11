15f5b51ffe68999df81a8debe959267b351072cc
vvjpet <juri.petersen@visuellverstehen.de>
2022-12-11 18:12:04 +0100

    Push test stub

--- src/QLearning.test.scala
+++ src/QLearning.test.scala
@@ -1,5 +1,3 @@
-
-
 //> using target { scope "test" }
 //> using scala "3.1.3"
 //> using lib "org.scalacheck::scalacheck:1.16.0"
@@ -7,7 +5,14 @@
 //> using lib "dev.optics::monocle-core:3.1.0"
 //> using lib "dev.optics::monocle-macro:3.1.0"
 
+package src
+
 import org.scalacheck.*
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Prop.*
 
+object QLearningSpec
+  extends org.scalacheck.Properties("QLearning"):
+
+  property("Ex01.00: No tests now. We test this in Exercise 3") =
+    true
